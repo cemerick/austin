96c7c011d27d8dc4894a431f799ac5f2dcd78ffb
Chas Emerick <chas@cemerick.com>
2013-08-02 15:37:28 -0400

    leiningen plugin/middleware

---
+++ src/clj/austin/plugin.clj
@@ -0,0 +1,22 @@
+(ns austin.plugin
+  (:require [clojure.java.io :as io]))
+
+(def ^:private austin-version
+  (-> (io/resource "META-INF/leiningen/com.cemerick/austin/project.clj")
+       slurp
+       read-string
+       (nth 2)))
+
+(assert (string? austin-version)
+        (str "Something went wrong, version of austin is not a string: "
+             austin-version))
+
+(defn middleware
+  [project]
+  (-> project
+      (update-in [:dependencies]
+                 (fnil into [])
+                 [['com.cemerick/austin austin-version]])
+      (update-in [:repl-options :nrepl-middleware]
+                 (fnil into [])
+                 '[cemerick.piggieback/wrap-cljs-repl])))
