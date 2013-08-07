ef4f77d712e4495b8cd8b3649f4909c9f98c49a8
Chas Emerick <chas@cemerick.com>
2013-08-07 10:41:26 -0400

    0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.cemerick/austin "0.1.0"
+(defproject com.cemerick/austin "0.1.1-SNAPSHOT"
   :description "The ClojureScript browser-repl, rebuilt stronger, faster, easier."
   :url "http://github.com/cemerick/austin"
   :license {:name "Eclipse Public License"
